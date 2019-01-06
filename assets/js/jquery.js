a54c9f6f0ffeb81b5102537eee36b800af4d4f83
Alee <aleesang20@Alees-MBP.gateway>
2019-01-06 14:24:24 +0930

    initialise files

---
+++ assets/js/jquery.js
@@ -0,0 +1,43 @@
+
+//fade in sections
+$(document).ready(function() {
+    $(window).scroll( function(){
+        $('.fadein').each( function(i){
+            
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            if( bottom_of_window > bottom_of_element ){
+                $(this).animate({'opacity':'1'},1000);
+            }
+            
+        }); 
+    });
+});
+
+//cookies
+window.addEventListener("load", function(){
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#bfadad",
+        "text": "#ffffff"
+  },
+    "button": {
+      "background": "#000000",
+        "text": "#e3c1c1"
+        }
+      },
+      "theme": "edgeless",
+      "content": {
+        "message": "We use cookies to ensure you get the best experience on our website.",
+        "dismiss": "GOT IT",
+        "link": "LEARN MORE"
+      }
+    })});
+
+  // fadein jumbotron
+  $(".opening").hide().fadeIn(2000);
+
+  
+ 
