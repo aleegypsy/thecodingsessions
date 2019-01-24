c23fbfbbec5b3917766f1b488b4271a340bc74d8
aleegypsy <aleesang20@Alees-MBP.gateway>
2019-01-25 04:40:26 +0930

    validated html and css and amended caption

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,20 +1,5 @@
 /* global $ */
 $(document).ready(function () {
-    
-//scroll down
-    $(function(){
-      $("a").on('click', function(event) {
-        if (this.hash !== "") {
-          event.preventDefault();
-          var hash = this.hash;
-          $('html, body').animate({
-            scrollTop: $(hash).offset().top
-          }, 800, function(){
-            window.location.hash = hash;
-          });
-        } 
-      });
-    });    
 
   
  // hide navbar on scroll 
