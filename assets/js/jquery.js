cb984eec5588a1f8e13e823cea5869d08e77deb7
aleegypsy <aleesang20@Alees-MBP.gateway>
2019-02-15 20:06:09 +0930

    commit all changes

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -74,11 +74,26 @@ new simpleParallax(image, {
 	transition: 'cubic-bezier(0,0,0,1)'
 });
 
-var image = document.getElementsByClassName('frontimage2');
-new simpleParallax(image, {
-	orientation: 'left'
-});
 
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+    
+      /* Check the location of each desired element */
+      $('.hideme').each( function(i){
+          
+          var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+          var bottom_of_window = $(window).scrollTop() + $(window).height();
+          
+          /* If the object is completely visible in the window, fade it it */
+          if( bottom_of_window > bottom_of_object ){
+              
+              $(this).animate({'opacity':'1'},500);
+                  
+          }
+          
+      }); 
+  
+  });
 
 
 });
