fced51ab69248038536b5750537898117e4d473c
aleegypsy <aleesang20@Alees-MBP.gateway>
2019-02-19 22:05:31 +0930

    re-branding

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -67,33 +67,7 @@ $(function() {
   });
 });
 
-// parallax on individual images
-var image = document.getElementsByClassName('frontimage');
-new simpleParallax(image, {
-	delay: .6,
-	transition: 'cubic-bezier(0,0,0,1)'
-});
 
-
-    /* Every time the window is scrolled ... */
-    $(window).scroll( function(){
-    
-      /* Check the location of each desired element */
-      $('.hideme').each( function(i){
-          
-          var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-          var bottom_of_window = $(window).scrollTop() + $(window).height();
-          
-          /* If the object is completely visible in the window, fade it it */
-          if( bottom_of_window > bottom_of_object ){
-              
-              $(this).animate({'opacity':'1'},500);
-                  
-          }
-          
-      }); 
-  
-  });
 
 
 });
