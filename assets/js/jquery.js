73b5429eabe6720cd80e99977a7d3d780d0bccb4
aleegypsy <aleesang20@Alees-MBP.gateway>
2019-02-25 08:46:41 +0930

    committing final changes

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -2,34 +2,34 @@
 $(document).ready(function () {
 
   
- // hide navbar on scroll 
+ // hide navbar on scroll
   $(function () {
     var lastScrollTop = 0;
     var $navbar = $('.navbar');
-  
+
     $(window).scroll(function(event){
       var st = $(this).scrollTop();
-  
+
       if (st > lastScrollTop) { // scroll down
-        
+
         // use this is jQuery full is used
         $navbar.fadeOut()
-        
+
         // use this to use CSS3 animation
         // $navbar.addClass("fade-out");
         // $navbar.removeClass("fade-in");
-        
+
         // use this if no effect is required
         // $navbar.hide();
       } else { // scroll up
-        
+
         // use this is jQuery full is used
         $navbar.fadeIn()
-        
+
         // use this to use CSS3 animation
         // $navbar.addClass("fade-in");
         // $navbar.removeClass("fade-out");
-        
+
         // use this if no effect is required
         // $navbar.show();
       }
@@ -38,7 +38,17 @@ $(document).ready(function () {
   });
 
 
-  // ===== Scroll to Top ==== 
+ // transparent to solid
+  $(window).scroll(function() {
+    // checks if window is scrolled more than 500px, adds/removes solid class
+    if($(this).scrollTop() > 500) { 
+        $('.navbar').addClass('solid');
+    } else {
+        $('.navbar').removeClass('solid');
+    }
+  });
+
+  // ===== Scroll to Top ====
 $(window).scroll(function() {
   if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
       $('#return-to-top').fadeIn(200);    // Fade in the arrow
@@ -52,20 +62,12 @@ $('#return-to-top').click(function() {      // When arrow is clicked
   }, 500);
 });
 
+  // ===== Background Set ====
+	$('.set-bg').each(function() {
+		var bg = $(this).data('setbg');
+		$(this).css('background-image', 'url(' + bg + ')');
+	});
 
-
-// transparent to solid navbar  
-$(function() {
-  // Transition effect for navbar 
-  $(window).scroll(function() {
-    // checks if window is scrolled more than 500px, adds/removes solid class
-    if($(this).scrollTop() > 500) { 
-        $('.navbar').addClass('solid');
-    } else {
-        $('.navbar').removeClass('solid');
-    }
-  });
-});
 
 
 
